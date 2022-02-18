2d6bd99d09f5e602f4535d8ff2055c65563b10ac
nhma <nhma@mmmi.sdu.dk>
2022-02-18 16:22:53 +0100

    loop images

--- finn_tester/finn_tester_node.py
+++ finn_tester/finn_tester_node.py
@@ -6,7 +6,6 @@ from sensor_msgs.msg import Image
 import numpy as np
 import os
 
-
 class FinnTesterNode(Node):
 
 	def __init__(self):
@@ -18,16 +17,35 @@ class FinnTesterNode(Node):
 		self.i = 0
 		self.path = "/home/mp4d/test_images"
 		self.filenames = os.listdir(self.path)
-
+		self.img_names = [
+			"000000298380",
+			"000000550467",
+			"000000568587",
+			"000000569414",
+			"000000292042",
+			"000000537124",
+			"000000542444",
+			"000000454566",
+			"000000562345",
+			"000000563575",
+			"2012_003846",
+			"000000428011",
+			"000000460235",
+			"000000550396",
+			"000000311436",
+			"2012_004283"			
+		]
+		
 	def timer_callback(self):
-		filename = self.filenames[self.i]
+		#filename = self.filenames[self.i]
 		# img = cv2.imread(os.path.join(self.path,filename),1)
-		img = cv2.imread(os.path.join(self.path,"0000002989380.jpg"),1)
+		filename_a = self.img_names[i] + ".jpg"
+		img = cv2.imread(os.path.join(self.path, filename_a),1)
 		#img = cv2.resize(img, (256,256), interpolation = cv2.INTER_AREA)
 		msg = self.bridge.cv2_to_imgmsg(np.array(img), "bgr8")
 		msg.header.frame_id = "camera"
 		self.publisher_.publish(msg)
-		self.i = (self.i + 1) % len(self.filenames)
+		self.i = (self.i + 1) % len(self.img_names)
 
 def main(args=None):
 	rclpy.init(args=args)
